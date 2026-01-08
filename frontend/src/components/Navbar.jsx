d66545bcbe23f967c835f71c804c702275e45ac2
Navyan <navyanchaitanya@gmail.com>
2026-01-08 22:19:36 +0530

    updated application logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = ({ token, userData, onLogout }) => {
       {/* Desktop Navbar */}
       <nav className="bg-white/80 backdrop-blur-lg border-b border-gray-200 px-6 py-4 shadow-sm sticky top-0 z-50">
         <div className="max-w-7xl mx-auto flex justify-between items-center">
-          {/* Logo */}
+          <img src="/public/icon2.png" alt="Logo" className="w-10 h-10" />
           <Link to="/" className="flex items-center space-x-2">
             <motion.div
               whileHover={{ rotate: 360 }}
@@ -46,7 +46,7 @@ const Navbar = ({ token, userData, onLogout }) => {
               <FaFileAlt className="text-white text-lg" />
             </motion.div>
             <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-cyan-600 bg-clip-text text-transparent">
-              ResumeIQ
+              
             </span>
           </Link>
 
